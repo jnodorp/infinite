492064d9b91069f270658ccfce9f2c523e32b55f
Julian Nodorp <julian.nodorp@mailbox.org>
2023-09-29 11:27:56 +0200

    fix: compatibility with Go 1.21

--- read_cgo.go
+++ read_cgo.go
@@ -11,14 +11,16 @@ import (
 	"unsafe"
 )
 
-var _ io.Reader = (*C.reader_t)(nil)
+type reader_t C.reader_t
+
+var _ io.Reader = (*reader_t)(nil)
 
 // NewReader that will always return data. Once all data has been read, it will continue from the start.
 func NewReader(data []byte) io.Reader {
 	data = bytes.Repeat(data, 16)
-	return C.new_reader(C.CBytes(data), C.ulong(len(data)))
+	return (*reader_t)(C.new_reader(C.CBytes(data), C.ulong(len(data))))
 }
 
-func (r *C.reader_t) Read(buf []byte) (int, error) {
-	return int(C._read(r, unsafe.Pointer(&buf[0]), C.ulong(len(buf)))), nil
+func (r *reader_t) Read(buf []byte) (int, error) {
+	return int(C._read((*C.reader_t)(r), unsafe.Pointer(&buf[0]), C.ulong(len(buf)))), nil
 }
